82930d5dc598bf96b1398656254572aa352f35aa
afnesss <af.nesss07@gmail.com>
2026-06-10 18:51:41 +0200

    feat: enhance DataMgmtPage with collapsible sections, search functionality, and improved styling

--- src/pages-layer/dashboard/ui/DashboardDataMgmtPage/DataMgmtPage.tsx
+++ src/pages-layer/dashboard/ui/DashboardDataMgmtPage/DataMgmtPage.tsx
@@ -2,34 +2,102 @@
 import { TicketSearchInput } from "@/src/features/search-tickets";
 import styles from "./DataMgmtPage.module.css";
 import TabSwitch from "@/src/shared/ui/TabSwitch/TabSwitch";
-import { useState } from "react";
-import { DashboardDateText, useI18n } from "@/src/shared";
+import { useEffect, useState } from "react";
+import { Button, DashboardCard, DashboardDateText, useI18n } from "@/src/shared";
 import MiniCalendarTrigger from "@/src/widgets/MiniCalendar/MiniCalendarTrigger";
+import { DataSection, MOCK_DATA_BY_TAB } from "./mockData";
+import { useSearch } from "@/src/shared/lib/useSearch";
+import DataTable from "@/src/shared/ui/DataTable/DataTable";
+import ChevronIcon from "@/src/shared/ui/ChevronIcon/ChevronIcon";
 
+type CollapsiblesectionProps = {
+  section: DataSection;
+  tab: string;
+  index: number;
+};
+const Collapsiblesection = ({ section, tab }: CollapsiblesectionProps) => {
+  const { t } = useI18n();
+  const [open, setOpen] = useState(false);
+
+  const handleToggle = () => {
+    setOpen((prev) => !prev);
+  };
+  return (
+    <>
+      <div>
+        <div className={styles.section} onClick={handleToggle}>
+          {section.title}
+          <ChevronIcon open={open} className={styles.chevron} />
+        </div>
+        {open && (
+          <DataTable
+            rows={section.rows ?? []}
+            columns={section.columns ?? []}
+            onAdd={() => {}}
+            onEdit={() => {}}
+          />
+        )}
+      </div>
+    </>
+  );
+};
 const DataMgmtPage = () => {
   const [tab, setTab] = useState("routes");
   const { t } = useI18n();
+  const [sections, setSections] = useState(MOCK_DATA_BY_TAB.routes.sections);
+
+  const { query, setQuery, filtered } = useSearch(sections, (section, q) => {
+    const lq = q.toLowerCase();
+
+    if (section.title.toLowerCase().includes(lq)) return true;
+
+    return (
+      section.rows?.some((row) => row.some((cell) => cell.toLowerCase().includes(lq))) ?? false
+    );
+  });
+
+  const handleTabChange = (value: string) => {
+    setTab(value);
+    setQuery("");
+  };
+
+  useEffect(() => {
+    const set = () => {
+      setSections(MOCK_DATA_BY_TAB[tab].sections);
+    };
+    set();
+  }, [tab]);
 
   const tabs = [
-    { value: "routes", label: t("dispatcherArea.settingsCards.dataAccess.items.routes") },
-    { value: "cafe", label: t("dispatcherArea.settingsCards.dataAccess.items.cafe") },
-    { value: "fleet", label: t("dispatcherArea.settingsCards.dataAccess.items.fleet") },
-    { value: "staff", label: t("dispatcherArea.settingsCards.dataAccess.items.staff") },
+    {
+      value: "routes",
+      label: t("dispatcherArea.settingsCards.dataAccess.items.routes"),
+      hasHeaderAction: true,
+    },
+    {
+      value: "fleet",
+      label: t("dispatcherArea.settingsCards.dataAccess.items.fleet"),
+      hasHeaderAction: false,
+    },
+    {
+      value: "staff",
+      label: t("dispatcherArea.settingsCards.dataAccess.items.staff"),
+      hasHeaderAction: false,
+    },
+    {
+      value: "cafe",
+      label: t("dispatcherArea.settingsCards.dataAccess.items.cafe"),
+      hasHeaderAction: true,
+    },
   ];
 
   return (
     <div className={styles.mainContainer}>
       <div className={styles.header}>
-        <TabSwitch
-          tabs={tabs}
-          value={tab}
-          onChange={(value) => {
-            setTab(value);
-          }}
-        />
+        <TabSwitch tabs={tabs} value={tab} onChange={handleTabChange} />
         <TicketSearchInput
-          onChange={() => {}}
-          value=""
+          onChange={setQuery}
+          value={query}
           placeholder={t("common.search.placeholder")}
         />
         <div className={styles.dateContainer}>
@@ -37,6 +105,28 @@ const DataMgmtPage = () => {
         </div>
         <MiniCalendarTrigger />
       </div>
+
+      <DashboardCard
+        title={tabs.find((i) => i.value === tab)?.label}
+        classNameHeader={styles.tableHeader}
+        headerAction={
+          tabs.find((i) => i.value === tab)?.hasHeaderAction && (
+            <Button
+              fullWidth={false}
+              variant="secondary"
+              text={t(`dispatcherArea.dataMgmt.actions.${tab}`)}
+            ></Button>
+          )
+        }
+      >
+        <div className={styles.sections}>
+          {filtered.map((section: DataSection, index: number) => (
+            <div key={`${tab}-${index}`}>
+              <Collapsiblesection section={section} tab={tab} index={index} />
+            </div>
+          ))}
+        </div>
+      </DashboardCard>
     </div>
   );
 };
--- src/shared/ui/DashboardComponents/DashboardCard.tsx
+++ src/shared/ui/DashboardComponents/DashboardCard.tsx
@@ -1,4 +1,4 @@
-import type { HTMLAttributes, ReactNode, Ref } from "react";
+import type { CSSProperties, HTMLAttributes, ReactNode, Ref } from "react";
 import SharedLabel from "../SharedLabel/SharedLabel";
 import styles from "./DashboardCard.module.css";
 
@@ -8,6 +8,7 @@ type DashboardCardProps = HTMLAttributes<HTMLDivElement> & {
   subtitle?: string;
   headerAction?: ReactNode;
   headerRef?: Ref<HTMLDivElement>;
+  classNameHeader?: string;
 };
 
 export default function DashboardCard({
@@ -16,6 +17,7 @@ export default function DashboardCard({
   title,
   subtitle,
   headerAction,
+  classNameHeader = "",
   headerRef,
   ...props
 }: DashboardCardProps) {
@@ -24,13 +26,11 @@ export default function DashboardCard({
   return (
     <div className={`${styles.card} ${className}`} {...props}>
       {hasHeader && (
-        <div ref={headerRef} className={styles.cardHeader}>
+        <div ref={headerRef} className={`${styles.cardHeader} ${classNameHeader}`}>
           {title && (
             <div className={styles.cardTitleGroup}>
               <SharedLabel variant="dashboardCardTitle">{title}</SharedLabel>
-              {subtitle && (
-                <SharedLabel variant="dashboardCardSubtitle">{subtitle}</SharedLabel>
-              )}
+              {subtitle && <SharedLabel variant="dashboardCardSubtitle">{subtitle}</SharedLabel>}
             </div>
           )}
           {headerAction}
