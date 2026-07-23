50c716c2b34846969a325d822e2fc45ebc7a29a4
afnesss <af.nesss07@gmail.com>
2026-07-23 13:33:24 +0200

    chore: fix build

--- src/entities/booking/index.ts
+++ src/entities/booking/index.ts
@@ -2,3 +2,8 @@ export * from "./api/bookings";
 export * from "./api/bookingApiKeys";
 export * from "./api/useBookingQueries";
 export * from "./model/types";
+export { default as BookingSummaryCard } from "./ui/BookingSummaryCard/BookingSummaryCard";
+export type {
+  BookingSummaryCardProps,
+  BookingSummaryStatusVariant,
+} from "./ui/BookingSummaryCard/BookingSummaryCard";
